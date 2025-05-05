182a9a29c80e319c25824b31f1e397a2ef4bb904
Jarif <xjarifx@gmail.com>
2025-05-05 22:02:37 +0600

    socket building ongoing

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,1 +1,7 @@
-console.log("haha");
+import express from "express";
+import http from "http";
+import socketIo from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
